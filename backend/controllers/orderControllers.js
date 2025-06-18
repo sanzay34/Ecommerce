6d878444ad0d04faf4847c4b385a0803b4795688
sanzay34 <sanzay321@gmail.com>
2025-06-18 19:31:41 +0545

    minor cahnges

--- backend/controllers/orderControllers.js
+++ backend/controllers/orderControllers.js
@@ -1,8 +1,14 @@
 import orderModel from "../models/orderModel.js"
 import userModels from "../models/userModels.js"
+import Esewa from 'esewajs'
 
+//global variables
 
+const currency = 'Rs.'
+const deliveryCharge=10
 
+//ggateway initialize
+const esewa = new Esewa(process.env.SECRET);
 //Placing order using COD method
 const placeOrder = async (req, res) => {
     try {
@@ -28,7 +34,50 @@ const placeOrderKhalti = async (req, res) => {
 
 //placing order using esewa
 const placeOrderEsewa = async (req, res) => {
-    
+    try {
+        const { userId, items, amount, address } = req.body;
+        const { origin } = req.headers
+        const orderData = {
+					userId,
+					address,
+					items,
+					amount,
+					paymentMethod: "esewa",
+					payment: false,
+					date: Date.now(),
+        };
+        const newOrder = new orderModel(orderData);
+        await newOrder.save();
+        const line_items = items.map((item) => ({
+            price_data: {
+                currency: currency,
+                product_data: {
+                    name:item.name
+                },
+                unit_amount:item.price *100
+            },quantity:item.quantity
+        }))
+        line_items.push({
+					price_data: {
+						currency: currency,
+						product_data: {
+							name: "Delivery Charge",
+						},
+						unit_amount: deliveryCharge * 100,
+					},
+					quantity: 1,
+        });
+        const session = await esewa.checkout.sessions.create({
+					succces_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+            line_items,
+                    mode:'payment',
+        });
+        res.json({success:true,session_url:session.url})
+    } catch (error) {
+        console.log(error);
+				res.json({ success: false, message: error.message });
+    }
         
 }
 
