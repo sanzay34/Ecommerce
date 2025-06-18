57a5508c9012d7ddc8ef5c0f9c197a28efaf2a08
sanzay34 <sanzay321@gmail.com>
2025-06-18 23:59:31 +0545

    minor changes

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { backendUrl } from '../App'
 import axios from 'axios'
+import { toast } from "react-toastify";
 const Login = ({setToken}) => {
     const [email,setEmail]=useState('')
     const [password,setPassword]=useState('')
