06b19b89002d08c26d1ae5358f9b93c810807165
sanzay34 <sanzay321@gmail.com>
2025-06-18 22:57:30 +0545

    baclend error fixed

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ app.use(cors(corsOptions));
 
 //api endpoints
 app.use('/api/user',userRouter)
-app.use('/api/product', productRouter)
+app.use('api/product', productRouter)
 app.use('/api/cart',cartRouter)
 app.use('/api/order', orderRouter)
 
